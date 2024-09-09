ce8757c39b3954bdd3ae5a83a51d8fb1a95ed9e6
Muriel <muriel.marchal@hotmail.com>
2024-09-09 16:50:22 +0200

    creation pages components for orders

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
+import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListOrdersComponent,
+    PageEditOrderComponent,
+    PageNotFoundComponent,
+    PageAddOrderComponent
+  ],
   imports: [
     CommonModule,
     OrdersRoutingModule
