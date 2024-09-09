4d3074290b656e0a2ac7530d4ac9c4f283bee735
Muriel <muriel.marchal@hotmail.com>
2024-09-09 16:31:03 +0200

    ajout modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { UiModule } from '../ui/ui.module';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [UiModule, IconsModule, TemplatesModule]
 })
 export class CoreModule { }
