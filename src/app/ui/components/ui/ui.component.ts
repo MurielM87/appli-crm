f0e55b8d1ba3cc9c6982d491df37f7bdca5bfbfd
Muriel <muriel.marchal@hotmail.com>
2024-09-10 16:29:59 +0200

    add new ui component and toggle btn for nav bar

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrl: './ui.component.scss'
+  styleUrl: './ui.component.scss',
 })
 export class UiComponent {
-
+  open = true; //public se met par défaut - open veut dire que le menu est ouvert
+  //public open: boolean = true;
+  toggle() { //public toggle() : void mais pas nécessaire
+    this.open = !this.open;
+    //console.log(`Open : ${this.open}`);
+  }
 }
