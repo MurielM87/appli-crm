2359c2bd5bd7b8d7a1673aa91fb63b98016ff918
Muriel <muriel.marchal@hotmail.com>
2024-09-10 12:28:16 +0200

    add component header, nav and footer in core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { UiModule } from '../ui/ui.module';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: [UiModule, IconsModule, TemplatesModule]
+  exports: [UiModule, IconsModule, TemplatesModule, HeaderComponent, NavComponent, FooterComponent]
 })
 export class CoreModule { }
