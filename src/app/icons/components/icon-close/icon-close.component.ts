41b175e85d3a21c779de1ebabe628b49cce01942
Muriel <muriel.marchal@hotmail.com>
2024-09-11 09:32:05 +0200

    modification html, scss, ts for icons and add close icon to app.component.html

--- src/app/icons/components/icon-close/icon-close.component.ts
+++ src/app/icons/components/icon-close/icon-close.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-close',
--- src/app/icons/components/icon-delete/icon-delete.component.ts
+++ src/app/icons/components/icon-delete/icon-delete.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-delete',
   templateUrl: './icon-delete.component.html',
-  styleUrl: './icon-delete.component.scss'
+  styleUrl: './icon-delete.component.scss',
 })
 export class IconDeleteComponent {
-
+  public myIcon = faTrash;
 }
