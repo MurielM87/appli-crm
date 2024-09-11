8b0ff8ad3b9377adcf76fd23172ab4dc1882edf0
Muriel <muriel.marchal@hotmail.com>
2024-09-11 16:33:26 +0200

    modification router for loginPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
-import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
-import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
-
 const routes: Routes = [
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: PageSignInComponent },
-  { path: 'sign-up', component: PageSignUpComponent },
-  { path: 'forgot', component: PageForgotPasswordComponent },
-  { path: 'reset', component: PageResetPasswordComponent },
+  { path: '', redirectTo: '/auth/sign-in', pathMatch: 'full' },
+  {path: 'auth',
+    loadChildren: () =>
+      import('./login/login.module').then(m => m.LoginModule),
+  },
   {
     path: 'orders',
     loadChildren: () =>
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
+import { PageForgotPasswordComponent } from './pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './pages/page-reset-password/page-reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sign-in', component: PageSignInComponent},
+  {path: 'sign-up', component: PageSignUpComponent},
+  {path: 'forgot', component: PageForgotPasswordComponent},
+  {path: 'reset', component: PageResetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
