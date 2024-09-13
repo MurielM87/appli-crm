32dce411fa526939d3cce7692ea0f2fe82c74fdc
Muriel <muriel.marchal@hotmail.com>
2024-09-13 11:59:22 +0200

    subject behaviorsubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mon CRM v1.0';
+
+  // // //cold and unicast observable
+  // // obs$ = new Observable(sub => {
+  // //   sub.next(Math.random());
+  // // });
+
+  // // constructor(){
+  // //   this.obs$.subscribe((data) => {
+  // //     console.log('a', data)
+  // //   });
+  // // }
+
+  // //hot and multicas
+  // sub$ = new Subject();
+  // constructor() {
+  //   //subscribe
+  //   this.sub$.subscribe((data) => {
+  //     console.log('a1', data);
+  //   });
+  //   //dans le next emission de flux de données
+  //   //sub$ ne retient que la dernière valeur (supprime l'ancienne valeur)
+  //   //pour enregistrer de nouveaux subscribe avec next()
+  //   this.sub$.next(Math.random());
+
+  //   //subscribe
+  //   this.sub$.subscribe((data) => {
+  //     console.log('a2', data);
+  //   });
+  //   //subscribe
+  //   this.sub$.subscribe((data) => {
+  //     console.log('a3', data);
+  //   });
+  // }
+
+  //Behaviour Subject - extension du comportement du subject - permet de se rattraper si retard
+  //toujours initialiser nouveau subject
+  sub$ = new BehaviorSubject(Math.random());
+  constructor() {
+    //subscribe
+    this.sub$.subscribe((data) => {
+      console.log('a1', data);
+    });
+    //subscribe
+    this.sub$.subscribe((data) => {
+      console.log('a2', data);
+    });
+    this.sub$.next(Math.random());
+    //subscribe
+    this.sub$.subscribe((data) => {
+      console.log('a3', data);
+    });
+  }
 }
